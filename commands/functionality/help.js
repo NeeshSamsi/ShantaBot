13fc04a7d268e1d4ebeb561e60fa1270db24a194
Neesh <avaneeshsamsi@gmail.com>
2022-10-22 23:31:24 +0530

    feat: Add help command

---
+++ commands/functionality/help.js
@@ -0,0 +1,48 @@
+const fs = require("fs")
+const path = require("path")
+
+module.exports = {
+  commands: "help",
+  description: "Lists all available commands and their uses.",
+  expectedArgs: "<command>",
+  permissionError: "You do not have permission to run this command.",
+  minArgs: 0,
+  maxArgs: 1,
+  callback: (message, arguments, text) => {
+    const baseFile = "command-base.js"
+
+    let allCommands = []
+
+    const readCommands = (dir) => {
+      const files = fs.readdirSync(dir)
+
+      for (const file of files) {
+        const stat = fs.lstatSync(path.join(dir, file))
+
+        if (stat.isDirectory()) {
+          readCommands(path.join(dir, file))
+        } else if (file !== baseFile) {
+          const options = require(path.join(dir, file))
+          allCommands.push(options)
+          console.log(options)
+        }
+      }
+    }
+
+    readCommands(path.join(__dirname, ".."))
+
+    const commands = allCommands.map((command) => {
+      return {
+        name: typeof command.commands === "string" ? command.commands : command.commands.join(" / "),
+        description: command.description,
+        expectedArgs: command.expectedArgs,
+      }
+    })
+
+    const helpMessage = commands.map((command) => `**${command.name}**: \n${command.description}`).join("\n\n")
+
+    message.reply(helpMessage)
+  },
+  permissions: [],
+  requiredRoles: [],
+}
