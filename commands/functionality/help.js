758d585a44c7471adb4a7fa68b18de3ddfc25bfe
Neesh <avaneeshsamsi@gmail.com>
2022-10-23 14:56:36 +0530

    feat: Help command with all cmds and specific cmd

--- commands/functionality/help.js
+++ commands/functionality/help.js
@@ -24,7 +24,6 @@ module.exports = {
         } else if (file !== baseFile) {
           const options = require(path.join(dir, file))
           allCommands.push(options)
-          console.log(options)
         }
       }
     }
@@ -33,15 +32,52 @@ module.exports = {
 
     const commands = allCommands.map((command) => {
       return {
-        name: typeof command.commands === "string" ? command.commands : command.commands.join(" / "),
+        name: command.commands,
         description: command.description,
         expectedArgs: command.expectedArgs,
       }
     })
 
-    const helpMessage = commands.map((command) => `\n\n**${command.name}**: \n${command.description}`).join("\n")
+    const commandToString = (commandArray) => {
+      if (!commandArray) return
+      if (typeof commandArray === "string") {
+        return commandArray
+      } else {
+        return commandArray.join(" / ")
+      }
+    }
 
-    message.reply(helpMessage)
+    const helpMessage = `\n\n**List of available commands :**\n\n${commands
+      .map((command) => `**${commandToString(command.name)}**: \n${command.description}`)
+      .join("\n\n")}\n\nFor specific information try: <@785469565227630592> help <command>`
+
+    const getSpecificHelpMessage = (command) => {
+      return `\n\`${commandToString(command.name)}\`\n**Description: **${
+        command.description
+      }\n**Usage: **<@785469565227630592> ${commandToString(command.name)} ${
+        command.expectedArgs ? command.expectedArgs : ""
+      }`
+    }
+
+    if (arguments[0]) {
+      const specificCommand = commands.filter((command) => {
+        if (typeof command.name === "string") {
+          return command.name === arguments[0]
+        } else {
+          for (const name of command.name) {
+            return name === arguments[0]
+          }
+        }
+      })[0]
+
+      if (!specificCommand) {
+        message.reply("That command does not exist. Try: <@785469565227630592> help for a list of available commands.")
+      } else {
+        message.reply(getSpecificHelpMessage(specificCommand))
+      }
+    } else {
+      message.reply(helpMessage)
+    }
   },
   permissions: [],
   requiredRoles: [],
