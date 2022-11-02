47de42e9a1f6a4a07e8a307f1cc13da5521d4708
Neesh <avaneeshsamsi@gmail.com>
2022-11-02 21:45:28 +0530

    feat: Load cmd handler in interaction create event

---
+++ events/interactions/slashCommands.js
@@ -0,0 +1,24 @@
+const { ChatInputCommandInteraction } = require("discord.js")
+
+module.exports = {
+  name: "interactionCreate",
+  /**
+   *
+   * @param {ChatInputCommandInteraction} interaction
+   */
+  execute(interaction, client) {
+    if (!interaction.isChatInputCommand()) return
+
+    const command = client.commands.get(interaction.commandName)
+
+    if (!command) {
+      return interaction.reply({ content: "This command is outdated.", ephemeral: true })
+    }
+
+    if (command.developer && interaction.user.id !== "308920733529276416") {
+      return interaction.reply({ content: "This command is only available to my owner, Neesh", ephemeral: true })
+    }
+
+    command.execute(interaction, client)
+  },
+}
