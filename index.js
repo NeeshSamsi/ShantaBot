8084fcb3c901f938dfde3d982a1a9b5cf7c28b31
Neesh <avaneeshsamsi@gmail.com>
2022-11-02 21:48:23 +0530

    feat Rm clutter, init client config & use handlers

--- index.js
+++ index.js
@@ -1,76 +1,20 @@
-import fs from "node:fs"
-import path from "node:path"
-import { fileURLToPath } from "node:url"
+const dotenv = require("dotenv")
 
-import { Client, Collection, Events, GatewayIntentBits } from "discord.js"
-import dotenv from "dotenv"
+const { Client, GatewayIntentBits, Partials, Collection } = require("discord.js")
+const { Guilds, GuildMembers, GuildMessages } = GatewayIntentBits
+const { User, Message, GuildMember, ThreadMember } = Partials
 
-dotenv.config()
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+const { loadEvents } = require("./handlers/eventHandler")
 
 const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent,
-    GatewayIntentBits.GuildMembers,
-  ],
+  intents: [Guilds, GuildMembers, GuildMessages],
+  partials: [User, Message, GuildMember, ThreadMember],
 })
 
+dotenv.config()
+client.events = new Collection()
 client.commands = new Collection()
 
-const commandsPath = path.join(__dirname, "commands")
+loadEvents(client)
 
-const readCommands = (dir) => {
-  const files = fs.readdirSync(dir)
-
-  for (const file of files) {
-    const filePath = path.join(dir, file)
-
-    const stat = fs.lstatSync(filePath)
-
-    if (stat.isDirectory()) {
-      readCommands(filePath)
-    } else {
-      const command = import(filePath)
-
-      if ("data" in command && "execute" in command) {
-        client.commands.set(command.data.name, command)
-      } else {
-        // console.log(`[WARNING] The command at ${filePath} is missing a required 'data' or 'execute' property.`)
-      }
-    }
-  }
-}
-
-readCommands(commandsPath)
-
-// When the client is ready, run this code (only once)
-// We use 'c' for the event parameter to keep it separate from the already defined 'client'
-client.once(Events.ClientReady, (c) => {
-  console.log(`Bot ready! Logged in as ${c.user.tag}`)
-})
-
-client.on(Events.InteractionCreate, async (interaction) => {
-  if (!interaction.isChatInputCommand) return
-
-  console.log(interaction)
-
-  const command = interaction.client.commands.get(interaction.commandName)
-
-  if (!command) {
-    console.error(`No command matching ${interaction.commandName} was found.`)
-  }
-
-  try {
-    await command.execute(interaction)
-  } catch (err) {
-    console.error(err)
-    await interaction.reply({ content: "There was an error while executing this command", ephemeral: true })
-  }
-})
-
-// Log in to Discord with your client's token
 client.login(process.env.TOKEN)
