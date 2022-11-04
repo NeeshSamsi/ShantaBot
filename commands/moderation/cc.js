97ea0fd654533cd13d63a5dd61c87a2058d63deb
Neesh <avaneeshsamsi@gmail.com>
2022-11-04 22:02:57 +0530

    feat: Add error handling for bulkDelete()

--- commands/moderation/cc.js
+++ commands/moderation/cc.js
@@ -1,4 +1,4 @@
-const { ChatInputCommandInteraction, SlashCommandBuilder, ChannelType } = require("discord.js")
+const { ChatInputCommandInteraction, SlashCommandBuilder, ChannelType, EmbedBuilder } = require("discord.js")
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -18,20 +18,29 @@ module.exports = {
    *
    * @param {ChatInputCommandInteraction} interaction
    */
-  async execute(interaction, client) {
+  async execute(interaction) {
     const amount = interaction.options.getInteger("number")
     const channelInput = interaction.options.getChannel("channel")
 
     const channel = channelInput ? channelInput : interaction.channel
 
-    await channel.bulkDelete(amount)
+    try {
+      await channel.bulkDelete(amount)
+    } catch (err) {
+      console.error(err)
+      const errorEmbed = new EmbedBuilder()
+        .setTitle("Error deleting messages : /cc")
+        .setDescription(err.rawError.message)
+      interaction.reply({ embeds: [errorEmbed], ephemeral: true })
+    }
+
     const reply = await interaction.reply({
-      content: `Deleted ${amount} message${amount > 1 ? "s" : ""}.\nShanta Bai sab saaf karegi`,
+      content: `Deleted ${amount} message${amount > 1 ? "s" : ""} from ${channel}.\nShanta Bai sab saaf karegi!`,
       fetchReply: true,
     })
     const msg = await channel.messages.fetch(reply.id)
     setTimeout(() => {
       msg.delete()
-    }, 2000)
+    }, 3500)
   },
 }
