7281401e271d78cd40d54b4594138dbedb77b67a
Neesh <avaneeshsamsi@gmail.com>
2022-11-05 16:31:19 +0530

    feat: Tested iterating through an object

--- test.js
+++ test.js
@@ -1,1 +1,16 @@
-console.log(process.platform)
+const commands = {
+  category1: [
+    { name: "sth", description: "desc" },
+    { name: "sth", description: "desc" },
+    { name: "sth", description: "desc" },
+  ],
+  category2: [
+    { name: "sth", description: "desc" },
+    { name: "sth", description: "desc" },
+    { name: "sth", description: "desc" },
+  ],
+}
+
+for (const category in commands) {
+  console.log(category)
+}
