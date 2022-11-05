184f764c2f6c50a34df987da10deb5f41b322643
Neesh <avaneeshsamsi@gmail.com>
2022-11-05 16:42:11 +0530

    feat: Completed about/help command

---
+++ commands/functionality/about.js
@@ -0,0 +1,66 @@
+const fs = require("node:fs")
+
+const { ChatInputCommandInteraction, SlashCommandBuilder, EmbedBuilder } = require("discord.js")
+
+const capitalize = require("../../functions/capitalize")
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("about")
+    .setDescription("Gives some information about Shanta and lists all available commands."),
+  /**
+   *
+   * @param {ChatInputCommandInteraction} interaction
+   */
+  execute(interaction, client) {
+    const helpEmbed = new EmbedBuilder()
+      .setColor("White")
+      .setTitle("Hi, I am Shanta Bai.")
+      .setDescription(
+        "I am the brain child of Neeshツ. I was forged by his intellect in desperate times to fill the void in his heart and bring purpose to his server. Now he pimps me out to be controlled and commanded by others. SOMEBODY HELPP MEE !!!"
+      )
+      .setThumbnail(client.user.avatarURL())
+      .setFooter({ text: "To use any of these commands, type /.", iconURL: client.user.avatarURL() })
+
+    const commands = {}
+    // const commands = {
+    //   category: [
+    //     { name, description},
+    //     { name, description},
+    //     { name, description},
+    //   ],
+    //   category: [
+    //     { name, description},
+    //     { name, description},
+    //     { name, description},
+    //   ]
+    // }
+
+    client.commands.forEach((command, commandName) => {
+      if (commandName === "reload") return
+
+      const { name, description } = command.data
+      const { category } = command
+
+      if (commands[category]) {
+        commands[category] = [...commands[category], { name, description }]
+      } else {
+        commands[category] = [{ name, description }]
+      }
+    })
+
+    for (const category in commands) {
+      const categoryDescription = fs
+        .readFileSync(`${process.cwd().replace(/\\/g, "/")}/commands/${category}/description.txt`)
+        .toString()
+
+      helpEmbed.addFields({ name: capitalize(category), value: categoryDescription })
+
+      for (const command of commands[category]) {
+        helpEmbed.addFields({ name: `/${command.name}`, value: command.description, inline: true })
+      }
+    }
+
+    interaction.reply({ embeds: [helpEmbed] })
+  },
+}
