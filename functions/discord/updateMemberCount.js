f8036f8e7b208117445edfe5424528001754b54a
Neesh <avaneeshsamsi@gmail.com>
2022-11-22 19:50:09 +0530

    Revert "fix: Remove console.log"
    This reverts commit 11ee53b12c70e868053511c3a41ab9eaca1084c4.

--- functions/discord/updateMemberCount.js
+++ functions/discord/updateMemberCount.js
@@ -10,6 +10,8 @@ const xata = getXataClient()
 module.exports = async (client) => {
   const guilds = await xata.db.Guild.getAll()
 
+  console.log(guilds)
+
   guilds.forEach(async ({ id, memberCountChannel }) => {
     const guild = await client.guilds.cache.get(id)
     const channel = guild.channels.cache.get(memberCountChannel)
