8b487ae399e97249e242b511de8d0d3e77257864
NeeshSamsi <avaneeshsamsi@gmail.com>
2024-01-10 23:26:22 +0530

    feat: Add message content intent

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const dotenv = require("dotenv")
 
-const { Client, GatewayIntentBits, Partials, Collection } = require("discord.js")
-const { Guilds, GuildMembers, GuildMessages } = GatewayIntentBits
+const {
+  Client,
+  GatewayIntentBits,
+  Partials,
+  Collection,
+} = require("discord.js")
+const { Guilds, GuildMembers, GuildMessages, MessageContent } =
+  GatewayIntentBits
 const { User, Message, GuildMember, ThreadMember } = Partials
 
 const { loadEvents } = require("./handlers/eventHandler")
 
 const client = new Client({
-  intents: [Guilds, GuildMembers, GuildMessages],
+  intents: [Guilds, GuildMembers, GuildMessages, MessageContent],
   partials: [User, Message, GuildMember, ThreadMember],
 })
 
