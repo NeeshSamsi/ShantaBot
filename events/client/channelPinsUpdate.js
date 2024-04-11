24eaa3432e0d6afb098f2d03370ef1ca65702d8a
NeeshSamsi <avaneeshsamsi@gmail.com>
2024-04-11 16:35:44 +0530

    feat: Disable hijack pinned messages

--- events/client/channelPinsUpdate.js
+++ events/client/channelPinsUpdate.js
@@ -17,51 +17,44 @@ module.exports = {
    */
   async execute(channel, _, client) {
     // Check if it's Server 2.0
-    if (channel.guildId !== "849545086718443520") return
-
-    console.log(`Pinned message in ${channel.name}`)
-
-    const server = await client.guilds.fetch("849545086718443520")
-    const pinsChannel = await server.channels.fetch("1194319244519809054")
-    const pinnedChannel = await server.channels.fetch(channel.id)
-    const pinnedMessagesFromChannel = await pinnedChannel.messages.fetchPinned()
-    const pinnedMessage = pinnedMessagesFromChannel.first()
-
-    if (!pinnedMessage.pinned) {
-      console.log("Message was unpinned, ignoring")
-      return
-    }
-
-    const { content, author, ...rest } = pinnedMessage
-
-    const link = getMessageLink(pinnedMessage)
-    const pinMessage = generatePinMessage({
-      content,
-      authorId: author.id,
-      link,
-    })
-
-    if (pinMessage.length > 2000) {
-      return pinnedChannel.send(
-        `${link}\nThis message was unable to be processed because it was too big.\nThat's what .. nvm\n\nIt will remain pinned.`
-      )
-    }
-
-    try {
-      const success = await pinsChannel.send(pinMessage)
-      await pinnedMessage.unpin()
-
-      pinnedMessage.reply(
-        `Cleaned up your shit once again.\nThis message was pinned here: ${getMessageLink(
-          success
-        )}`
-      )
-
-      console.log(`Handled pinned message in ${channel.name}`)
-    } catch (error) {
-      pinnedMessage.reply(
-        "Something went wrong trying to process this message. It will remain pinned."
-      )
-    }
+    // if (channel.guildId !== "849545086718443520") return
+    // const server = await client.guilds.fetch("849545086718443520")
+    // const pinsChannel = await server.channels.fetch("1194319244519809054")
+    // setTimeout(async () => {
+    //   const allPinnedMessages = await channel.messages.fetchPinned()
+    //   const pinnedMessage = allPinnedMessages.first()
+    //   if (pinnedMessage.pinned) {
+    //     console.log(`Pinned message in ${channel.name}`)
+    //   } else {
+    //     console.log(`Message in ${channel.name} was unpinned, ignoring`)
+    //     return
+    //   }
+    //   const { content, author, ...rest } = pinnedMessage
+    //   const link = getMessageLink(pinnedMessage)
+    //   const pinMessage = generatePinMessage({
+    //     content,
+    //     authorId: author.id,
+    //     link,
+    //   })
+    //   if (pinMessage.length > 2000) {
+    //     return pinnedChannel.send(
+    //       `${link}\nThis message was unable to be processed because it was too big.\nThat's what .. nvm\n\nIt will remain pinned.`
+    //     )
+    //   }
+    //   try {
+    //     const success = await pinsChannel.send(pinMessage)
+    //     await pinnedMessage.unpin()
+    //     pinnedMessage.reply(
+    //       `Cleaned up your shit once again.\nThis message was pinned here: ${getMessageLink(
+    //         success
+    //       )}`
+    //     )
+    //     console.log(`Handled pinned message in ${channel.name}`)
+    //   } catch (error) {
+    //     pinnedMessage.reply(
+    //       "Something went wrong trying to process this message. It will remain pinned."
+    //     )
+    //   }
+    // }, 500)
   },
 }
